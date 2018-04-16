5de0506171aa5c91515df3ba932c47536fb6d4a1
Mariann Javorszky <mariann.javorszky@aimotive.com>
2018-04-16 11:33:06 +0200

    exceptions modul completed

--- 02-exceptions/02-exceptions/02-exceptions.cpp
+++ 02-exceptions/02-exceptions/02-exceptions.cpp
@@ -2,25 +2,48 @@
 
 #include <iostream>
 #include <string>
+#include <exception>
 
 void somethingWentWrong();
 
 void moreErrorsOccured();
 
+class ExceptionClass : public std::exception
+{
+public:
+	virtual const char* what() const
+	{
+		return "Something bad happened.";
+	}
+};
+
+class Test
+{
+public:
+	void goesWrong() {
+		bool error = true;
+
+		if (error)
+		{
+			throw std::bad_alloc();
+		}
+	}
+};
 
 int main()
 {
 	try
 	{
-		somethingWentWrong();
+		moreErrorsOccured();
 	}
-	catch (int e)
+	// right order of catching exceptions starting from child class to parent class
+	catch (ExceptionClass &e)
 	{
-		std::cout << "ERROR" << e << " has occured." << std::endl;
+		std::cout << e.what() << std::endl;
 	}
-	catch (std::string e)
+	catch (std::exception &e)
 	{
-		std::cout << e << std::endl;
+		std::cout << e.what() << std::endl;
 	}
 
     return 0;
@@ -28,17 +51,17 @@ int main()
 
 void somethingWentWrong()
 {
-	bool error1 = false;
+	bool error1 = true;
 	bool error2 = true;
 
 	if (error1)
 	{
-		throw 1;
+		throw ExceptionClass();
 	}
 
 	if (error2)
 	{
-		throw std::string("A string error has occured");
+		throw std::exception();
 	}
 }
 
